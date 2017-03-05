e0fb7fc79fa9b492c04b620de42ba37c25c71792
LukeKedz <lukekedziora@gmail.com>
2017-03-05 10:39:18 -0500

    refactored scrape method; pulling entire stats table

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,34 +1,30 @@
-class SiteController < ApplicationController
+  class SiteController < ApplicationController
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-  def welcome
+  def scrape
     page = HTTParty.get('http://games.espn.com/fhl/standings?leagueId=8266&seasonId=2017')
 
-    categories = Nokogiri::HTML(page).css('#statsTable tr:nth-child(3) a')
+    categories  = Nokogiri::HTML(page).css('#statsTable tr:nth-child(3) a')
     @categories = []
-    categories.each do |row|
-      @categories.push(row.text)
-    end
+    categories.each { |category| @categories.push category.text }
 
-    lk = Nokogiri::HTML(page).css('#statsTable tr:nth-child(4) td')
-    @lk = []
-    lk.each_with_index do |row, index|
-      puts index.to_s + " " + row.to_s
-      if index != 2 && index != 12 && index != 18 && index != 19 && index != 20
-        @lk.push(row.text)
-      end
-    end
+    @stats = []
+    skip   = [2, 12, 18, 19, 20]
 
-    mk = Nokogiri::HTML(page).css('#statsTable tr:nth-child(5) td')
-    @mk = []
-    mk.each_with_index do |row, index|
-      puts index.to_s + " " + row.to_s
-      if index != 2 && index != 12 && index != 18 && index != 19 && index != 20
-        @mk.push(row.text)
+    i = 4
+    while i <= 13
+      nth_row = i.to_s
+      row = Nokogiri::HTML(page).css('#statsTable tr:nth-child(' + nth_row + ') td')
+
+      team_data = []
+      row.each_with_index do |data, index|
+        if !skip.include? index then team_data.push data.text end
       end
+
+      @stats.push team_data
+      i += 1
     end
-    
   end
 
 end
