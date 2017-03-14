a9e68b6d051861a1ff4727382dd4cac7d0c16731
LukeKedz <lukekedziora@gmail.com>
2017-03-14 11:33:01 -0400

    repo name switch from Zamboni to Spectrum

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store :cookie_store, key: '_zamboni_session'
+Rails.application.config.session_store :cookie_store, key: '_spectrum_session'
