0f3f7d4bbde3269e2efb90c3ba27cff0165a567e
LukeKedz <lukekedziora@gmail.com>
2017-04-14 14:58:53 -0400

    cleanup

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -61,46 +61,6 @@
     end
   end
 
-  def scrape
-    # page = HTTParty.get('http://games.espn.com/fhl/standings?leagueId=8266&seasonId=2017')
-
-    # categories  = Nokogiri::HTML(page).css('#statsTable tr:nth-child(3) a')
-    # @categories = []
-    # categories.each { |category| @categories.push category.text }
-
-    # @team  = Team.find(1)
-    # @stats = Statistic.where(team_id: 1)
-
-    # @stats = Statistic.all
-    # @stats = [[0,0],[100,150],[225,325],[336,563],[455,880],[609,420],[722,211]]
-
-    @stats = []
-
-    # http://chartkick.com/
-    # http://stackoverflow.com/questions/27590771/rails-chartkick-want-only-integer-values-on-axes-use-discrete-or-something-els
-    Statistic.where(team_id: 1).each do |row|
-      # @category = []
-      # @category.push [row.created_at.to_s[5..9], row.g]
-      @stats.push [row.created_at.to_s[5..9], row.g]
-    end
-
-    # @stats.push @category
-
-    # Statistic.where(team_id: 1).each do |row|
-    #   @category = []
-    #   @category.push [row.created_at.to_s[5..9], row.a]
-    # end
-
-    # @stats.push @category
-
-    # iterate for set min/max from data
-    @sk_min = 100
-    @sk_max = 5000
-
-    @g_min = 0
-    @g_max = 125
-  end
-
   def stats_upload
     # json_req = nil
 
