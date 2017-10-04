6f11c3ea3f95d3de5e2dab2228107622a26c0e88
LukeKedz <lukekedziora@gmail.com>
2017-10-04 08:26:02 -0400

    team update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@ Team.create(abbr: "KEDZ", name: "Elko Bugles")
 Team.create(abbr: "GS",   name: "General Salami")
 Team.create(abbr: "PBT",  name: "Philadelphia BallTappers")
 Team.create(abbr: "COD",  name: "Cod Brind'amour")
-Team.create(abbr: "SKUL", name: "Cass O'Wary's")
+Team.create(abbr: "SKUL", name: "Uni Voltines")
 Team.create(abbr: "FOI",  name: "Foxes On Ice")
 Team.create(abbr: "FLUX", name: "STATE  OF FLUX")
 Team.create(abbr: "CUP",  name: "That's Why We Fucking Play")
 Team.create(abbr: "SDET", name: "SD Belle")
+Team.create(abbr: "RUBE", name: "Gudas Smash")
+Team.create(abbr: "wahh", name: "boring uninspired")
 
 # ga_goals route
 # goals      = 0
