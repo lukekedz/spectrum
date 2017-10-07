d004242e26cd394286ba8718a315e6b3e4ff2f4c
LukeKedz <lukekedziora@gmail.com>
2017-10-07 09:35:18 -0400

    experimenting with get response

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
   class SiteController < ApplicationController
-  before_action :ip_authorized?, only: [:last_upload, :stats_upload, :last_twelve]
+  before_action :ip_authorized?, only: [:last_upload, :stats_upload]
   skip_before_filter :verify_authenticity_token, only: :stats_upload
 
   def root
